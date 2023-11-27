b0695046a10ea7017a354555101d52eb2f89c91f
JG-SSYT <jesmine_goh@connect.ite.edu.sg>
2023-11-28 03:25:49 +0800

    limit user from buying more in case others want

--- javascript/ticket.js
+++ javascript/ticket.js
@@ -1,3 +1,5 @@
+let totalPurchasedTickets = 0;
+
 // Add event listeners to the increment and decrement buttons
 document.querySelectorAll('.dec').forEach(btn => {
     btn.addEventListener('click', () => {
@@ -11,8 +13,17 @@ document.querySelectorAll('.dec').forEach(btn => {
     btn.addEventListener('click', () => {
       const inputField = btn.closest('.box').querySelector('.input-field');
       const value = parseInt(inputField.value, 10) + 1;
+  
+      // Check if the total tickets exceed the limit
+      if (totalPurchasedTickets + value > 4) {
+        alert('You have reached the maximum ticket purchase limit.');
+        return;
+      }
+  
       inputField.value = value;
+      totalPurchasedTickets += value; // Update the total purchased tickets
     });
   });
+  
   
   
