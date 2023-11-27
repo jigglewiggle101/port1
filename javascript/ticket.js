de321f9fb4fe76152b33df200906ed815b8ac820
JG-SSYT <jesmine_goh@connect.ite.edu.sg>
2023-11-28 04:14:23 +0800

    change functionality such as registration

--- javascript/ticket.js
+++ javascript/ticket.js
@@ -24,6 +24,49 @@ document.querySelectorAll('.dec').forEach(btn => {
       totalPurchasedTickets += value; // Update the total purchased tickets
     });
   });
+
+  registerUser(name, email, password);
+window.location.href = "/registration-success";
   
+  // User registration function
+function registerUser(name, email, password) {
+  // Hash the password for secure storage
+  const hashedPassword = bcrypt.hashSync(password, 10);
+
+  // Store user data in a database or local storage
+  const userData = {
+    name,
+    email,
+    hashedPassword
+  };
+  localStorage.setItem(`user-${email}`, JSON.stringify(userData));
+}
+
+
+
+// Ticket purchase function
+function purchaseTickets(userId, eventId, quantity) {
+  // Retrieve user data from storage
+  const userString = localStorage.getItem(`user-${userId}`);
+  const userData = JSON.parse(userString);
+
+  // Verify user credentials
+  if (!userData || !bcrypt.compareSync(password, userData.hashedPassword)) {
+    return false;
+  }
+
+  // Check if user has reached the purchase limit
+  const purchasedTickets = getUserPurchasedTickets(userId);
+  if (purchasedTickets + quantity > 3) {
+    alert('You have reached the maximum ticket purchase limit.');
+    return false;
+  }
+
+  // Update user's purchased tickets
+  updateUserPurchasedTickets(userId, quantity);
+
+  // Process ticket purchase (e.g., update database, send confirmation email)
+  return true;
+}
   
   
