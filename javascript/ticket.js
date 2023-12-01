5e515ed3fd7de178833fd8ebbe6f4148377c245a
JG-SSYT <jesmine_goh@connect.ite.edu.sg>
2023-12-01 23:36:52 +0800

    functionality changes

--- javascript/ticket.js
+++ javascript/ticket.js
@@ -1,72 +1,49 @@
-let totalPurchasedTickets = 0;
 
-// Add event listeners to the increment and decrement buttons
-document.querySelectorAll('.dec').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const inputField = btn.closest('.box').querySelector('.input-field');
-      const value = parseInt(inputField.value, 10) - 1;
-      inputField.value = value < 0 ? 0 : value;
-    });
-  });
-  
-  document.querySelectorAll('.inc').forEach(btn => {
-    btn.addEventListener('click', () => {
-      const inputField = btn.closest('.box').querySelector('.input-field');
-      const value = parseInt(inputField.value, 10) + 1;
-  
-      // Check if the total tickets exceed the limit
-      if (totalPurchasedTickets + value > 4) {
-        alert('You have reached the maximum ticket purchase limit.');
-        return;
-      }
-  
-      inputField.value = value;
-      totalPurchasedTickets += value; // Update the total purchased tickets
-    });
+
+// Example logic - ticket.js
+
+// Get all quantity input fields
+const quantityInputs = document.querySelectorAll('.input-field');
+
+// Function to update total amount
+function updateTotalAmount() {
+  let total = 0;
+
+  // Calculate total based on quantity and price (assuming price is fixed)
+  quantityInputs.forEach(input => {
+    const quantity = parseInt(input.value);
+    total += quantity * 100; // Assuming each ticket costs 100 units
   });
 
-  registerUser(name, email, password);
-window.location.href = "/registration-success";
-  
-  // User registration function
-function registerUser(name, email, password) {
-  // Hash the password for secure storage
-  const hashedPassword = bcrypt.hashSync(password, 10);
-
-  // Store user data in a database or local storage
-  const userData = {
-    name,
-    email,
-    hashedPassword
-  };
-  localStorage.setItem(`user-${email}`, JSON.stringify(userData));
+  // Update the total amount field
+  const totalAmountField = document.getElementById('total-amount');
+  totalAmountField.value = total;
 }
 
+// Event listeners for increment and decrement buttons
+const incButtons = document.querySelectorAll('.inc');
+const decButtons = document.querySelectorAll('.dec');
+
+incButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    quantityInputs[index].value = parseInt(quantityInputs[index].value) + 1;
+    updateTotalAmount();
+  });
+});
+
+decButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    const currentValue = parseInt(quantityInputs[index].value);
+    if (currentValue > 0) {
+      quantityInputs[index].value = currentValue - 1;
+      updateTotalAmount();
+    }
+  });
+});
+
+// Initialize total amount on page load
+updateTotalAmount();
 
 
-// Ticket purchase function
-function purchaseTickets(userId, eventId, quantity) {
-  // Retrieve user data from storage
-  const userString = localStorage.getItem(`user-${userId}`);
-  const userData = JSON.parse(userString);
 
-  // Verify user credentials
-  if (!userData || !bcrypt.compareSync(password, userData.hashedPassword)) {
-    return false;
-  }
-
-  // Check if user has reached the purchase limit
-  const purchasedTickets = getUserPurchasedTickets(userId);
-  if (purchasedTickets + quantity > 3) {
-    alert('You have reached the maximum ticket purchase limit.');
-    return false;
-  }
-
-  // Update user's purchased tickets
-  updateUserPurchasedTickets(userId, quantity);
-
-  // Process ticket purchase (e.g., update database, send confirmation email)
-  return true;
-}
-  
   
